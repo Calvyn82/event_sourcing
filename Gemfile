8d37433dd43c42030950d14cc3f05bfefdb7a85d
Calvyn82 <claytonflesher@gmail.com>
2016-03-09 20:51:31 -0600

    Initial tests up and running

--- Gemfile
+++ Gemfile
@@ -3,4 +3,11 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'pg'
 
+gem 'minitest'
+gem 'rack'
+gem 'rack-test'
+gem 'guard'
+gem 'guard-minitest'
+gem 'foreman'
+
 gem 'shotgun'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/pride'
+require 'minitest/benchmark'
+require 'rack/test'
+require_relative '../test.rb'
+
+include Rack::Test::Methods
+
+def app
+  Sinatra::Application
+end
